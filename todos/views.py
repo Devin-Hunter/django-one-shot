5b2624bcae7ae648ec28f716588de7b57a5fe98a
Devin-Matherne <devinmatherne@ymail.com>
2023-12-13 13:37:36 -0700

    Feature 10 Complete

--- todos/views.py
+++ todos/views.py
@@ -29,3 +29,18 @@ def todo_list_create(request):
         "form": form,
     }
     return render(request, "todos/create.html", context)
+
+def todo_list_update(request, id):
+    todo_list = get_object_or_404(TodoList, id=id)
+    if request.method == "POST":
+        form = CreateForm(request.POST, instance=todo_list)
+        if form.is_valid():
+            form.save()
+            return redirect("todo_list_detail", id=id)
+    else:
+        form = CreateForm(instance=todo_list)
+    context = {
+        "form": form,
+        "todo_list": todo_list
+    }
+    return render(request, "todos/update.html", context)
